ee73f9938e57ceaae7943555b6b7d6163c249114
Jacob Pieczynski <jpieczynski2021@gmail.com>
2024-04-12 10:43:35 -0700

    create team rosters

--- src/const.py
+++ src/const.py
@@ -1,1 +1,1 @@
-ROSTER = {year: dict() for year in range(2008, 2024 + 1)} # End year not inclusive, +1 just symbolizes this instead of going to 2025
+ROSTER = {year: dict() for year in range(2023, 2024 + 1)} # End year not inclusive, +1 just symbolizes this instead of going to 2025
--- src/main.py
+++ src/main.py
@@ -1,10 +1,26 @@
 from const import *
+from pybaseball import *
+pybaseball.cache.enable()
 
 def main():
     print('Hello Game Prediction V3.0')
     # Load Rosters by year
     for year in ROSTER:
-        print(year)
+        dates = (f'{year}-01-01', f'{year}-12-31')
+        data = batting_stats_range(dates[0], dates[1]).reset_index()
+        for idx in range(len(data)):
+            row = data.loc[idx]
+            teams = row.loc['Tm'].split(',')
+            for team in teams:
+                if team not in ROSTER[year]:
+                    ROSTER[year][team] = dict()
+                ROSTER[year][team][row.loc['Name']] = row.loc['mlbID'] # Add player to team   
+
+    # Print Roster
+    for year in ROSTER:
+        for team in ROSTER[year]:
+            print(ROSTER[year][team])
+
     # Load Games
         # Load Stats for each team leading up to game
         # Compare stats and determine outcome
